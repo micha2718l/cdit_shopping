14973b99b04f14cff253412b096ad39cedc17000
Michael Haas <micha2718l@gmail.com>
2020-01-26 21:40:53 +0000

    initial update modal html

--- resources/views/list.php
+++ resources/views/list.php
@@ -5,18 +5,21 @@
 	<title>Shopping List - View List</title>
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <script src="/js/angular.min.js"></script>
+    <script src="/js/jquery-3.4.1.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
 </head>
 
 <body ng-app="listApp">
 
     <div ng-controller="listController as ctrl">
-        {{ ctrl.status }}
+        {{ ctrl.status }} / {{ ctrl.updateItem }}
         <table class="table table-striped">
             <thead class="thead-dark">
                 <tr>
                     <th>ID</th>
                     <th>List ID</th>
                     <th>Item</th>
+                    <th>Update?</th>
                     <th>Delete?</th>
                 </tr>
             </thead>
@@ -24,6 +27,10 @@
                 <td>{{ item.id }}</td>
                 <td>{{ item.list_id }}</td>
                 <td>{{ item.item }}</td>
+                <td>
+                    <button type="button" class="btn btn-primary" ng-click="ctrl.setUpdateItem(item)"
+                            data-toggle="modal" data-target="#myModal">Update</button>
+                </td>
                 <td>
                     <button type="button" class="btn btn-primary" ng-click="ctrl.delete(item.id)">Delete</button>
                 </td>
@@ -39,6 +46,31 @@
         <form>
             <button type="button" class="btn btn-primary" ng-click="ctrl.refreshList(1)">Refresh</button>
         </form>
+
+
+        <!-- Modal -->
+        <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Update Item (ID: {{ ctrl.updateItem.id }})</h4>
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+            </div>
+            <div class="modal-body">
+                <p>Original Item: {{ ctrl.updateItem.item }}</p>
+                <p>New Item: <input type="text" ng-model="ctrl.updateItem.newItem" />
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-dismiss="modal" ng-click="ctrl.update()">Update</button>
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            </div>
+            </div>
+
+        </div>
+        </div>
+
     </div>
     <script src="/js/ang_app.js"></script>
 
