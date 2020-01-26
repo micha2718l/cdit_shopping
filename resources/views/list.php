b5517751fbf03543bc813fe18a5334ccf47eeeff
Michael Haas <micha2718l@gmail.com>
2020-01-26 07:09:42 +0000

    add initial list view php, loading angular

---
+++ resources/views/list.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Shopping List - View List</title>
+    <script src="/js/angular.min.js"></script>
+</head>
+
+<body ng-app="listApp">
+
+    <div ng-controller="listController as ctrl">
+        {{ ctrl.value }}
+    </div>
+
+    <script src="/js/ang_app.js"></script>
+
+</body>
+</html>
