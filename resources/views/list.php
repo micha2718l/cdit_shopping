4a57d8bab968e8406fb7e0cf968a6d03b8639406
Michael Haas <micha2718l@gmail.com>
2020-01-26 21:07:22 +0000

    bootstrap plus delete

--- resources/views/list.php
+++ resources/views/list.php
@@ -3,25 +3,42 @@
 <head>
 	<meta charset="utf-8">
 	<title>Shopping List - View List</title>
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
     <script src="/js/angular.min.js"></script>
 </head>
 
 <body ng-app="listApp">
 
     <div ng-controller="listController as ctrl">
-        {{ ctrl.value }}
-        <div ng-repeat="item in ctrl.list">
-            {{ item.list_id }} - {{ item.item }}
-        </div>
+        {{ ctrl.status }}
+        <table class="table table-striped">
+            <thead class="thead-dark">
+                <tr>
+                    <th>ID</th>
+                    <th>List ID</th>
+                    <th>Item</th>
+                    <th>Delete?</th>
+                </tr>
+            </thead>
+            <tr ng-repeat="item in ctrl.list">
+                <td>{{ item.id }}</td>
+                <td>{{ item.list_id }}</td>
+                <td>{{ item.item }}</td>
+                <td>
+                    <button type="button" class="btn btn-primary" ng-click="ctrl.delete(item.id)">Delete</button>
+                </td>
+            </tr>
+        </table>
 
         <form>
             <input type="text" ng-model="ctrl.inputs.list_id" />
             <input type="text" ng-model="ctrl.inputs.item" />
 
-            <button type="button" class="btn" ng-click="ctrl.addItem()">Add</button>
-            <button type="button" class="btn" ng-click="ctrl.refreshList(1)">Refresh</button>
+            <button type="button" class="btn btn-primary" ng-click="ctrl.addItem()">Add</button>
         </form>
-
+        <form>
+            <button type="button" class="btn btn-primary" ng-click="ctrl.refreshList(1)">Refresh</button>
+        </form>
     </div>
     <script src="/js/ang_app.js"></script>
 
